4cff7ba6a98f937d6e2807c6b2ad7f50ebb69308
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-01-12 15:41:17 +0545

    Create script5.js

---
+++ script5.js
@@ -0,0 +1,11 @@
+       function changeColor(){
+            var par=document.getElementById("bgChange");
+            par.style.backgroundColor="red";
+      // document.getElementById("bgChange").
+        // style.backgroundColor="red";
+        }
+        function changetColor()
+          {
+         document.getElementById("bgChange").
+          style.color="blue";
+          }      
