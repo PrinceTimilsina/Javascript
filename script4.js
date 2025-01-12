a9644d6296ec5703aeb69443f87a65d54333632b
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-01-12 14:55:27 +0545

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,6 @@
+    function assignnum(){
+        var x=parseInt(prompt("Enter a number"));
+        var y=parseInt(prompt("Enter second number"));
+        var z=x+y;
+        alert(`sum is ${z}`);
+    }
