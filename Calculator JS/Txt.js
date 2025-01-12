fc5198712145690acef074e4198c2ce51668e380
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-01-12 15:46:09 +0545

    Delete Calculator JS directory

--- Calculator JS/Txt.js
+++
@@ -1,9 +0,0 @@
-    function dateCurrent ()
-         {
-        var currentDate=new Date();
-        var day=currentDate.getDate();
-        var mon=currentDate.getMonth() +1;
-        var yr=currentDate.getFullYear();
-        var formattedDate= (mon<10?'0':'')+mon+'/'+(day<10?'0':'')+day+'/'+yr;
-        alert(`Today date is:  ${formattedDate}`);
-        }
