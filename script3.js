2d237fe353ec17ad0e650267c7765987933ffa9e
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-01-12 14:54:28 +0545

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,7 @@
+  function sumCheck(){
+      var x=parseInt(prompt("Enter a number"));
+      var y=parseInt(prompt("Enter a number"));
+      var sum=x+y;
+      var display=`${sum} is the required value`
+      document.getElementById("sumEx")
+      .innerHTML=display;
