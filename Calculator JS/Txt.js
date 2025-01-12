a1b85aee872731c50501a3172b02a2f08ffa77d2
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-01-12 13:48:36 +0545

    Update Txt.js

--- Calculator JS/Txt.js
+++ Calculator JS/Txt.js
@@ -1,1 +1,9 @@
-
+    function dateCurrent ()
+         {
+        var currentDate=new Date();
+        var day=currentDate.getDate();
+        var mon=currentDate.getMonth() +1;
+        var yr=currentDate.getFullYear();
+        var formattedDate= (mon<10?'0':'')+mon+'/'+(day<10?'0':'')+day+'/'+yr;
+        alert(`Today date is:  ${formattedDate}`);
+        }
