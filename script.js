b4b495a7dd5cf02132254805f6b50e2228324eda
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-15 08:31:48 +0545

    JS-Today

---
+++ script.js
@@ -0,0 +1,54 @@
+// var a ="Prince";
+// console.log(a);
+// var a =10;
+// console.log(a);
+// var a=10-5;
+// console.log(a);
+
+// var a=10;
+// var b=3;
+// console.log(a/b);
+// console.log(a%b);
+
+// alert("Hello there");
+// var ans = confirm("Are you an adult");
+// console.log(ans);
+// var ans = prompt('enter you name');
+// console.log(ans);
+
+//js data types : primitive ; (number ,string ,boolean,undefined,null,symbol,NAN),reference(Array,Objects,functions);
+// var a;  //undefinned
+// console.log(a);
+
+// var a=10;
+// var b="prince";
+// console.log(a*b);//NaN
+
+// var a =10;
+// var b="Prince"
+// console.log(a+b)//10Prince  ,
+
+// if(condition){
+
+// }
+// var a = prompt("Enter a number");
+// if(a%5==0){
+//     console.log("Entered number is divisible by 5");
+// }
+// else{
+//     console.log("Entered number is not divisible by 5");
+// }
+
+// const n = prompt("Enter a number");
+// for(i=1;i<=10;i++){
+//    var c=n*i;
+//    console.log(c);
+// }
+
+// function date(){
+//     console.log("Today is 15th JAN-2025");
+// }
+// date();
+
+// var a =[23,4,5,6,6,7,]
+// console.log(a);  //Array
