6e2b195ac571cfde5e1727d27ad320b1b5d0ab5b
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-15 09:45:52 +0545

    NEW----LEARNING

--- script.js
+++ script.js
@@ -51,4 +51,100 @@
 // date();
 
 // var a =[23,4,5,6,6,7,]
+//index:  0  1 2 3 4 5 
 // console.log(a);  //Array
+
+// var a=[10,20,4,5,6,6]
+// console.log(a[4]);//6
+
+// var arr=[1,2,3,4,5,6,7];
+// console.log(arr);
+// arr.push(99);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// console.log(arr.length)
+
+// var arr = [10,20,30,40]
+// arr.forEach(function(elem){
+//     console.log('hello',elem)
+// })
+
+//------------OBJECTS--------
+
+// ()=function
+// {}=Object;
+// []=array
+
+// var obj = {
+//     user:'Prince Timilsina',
+//     age:15
+// }
+// console.log(obj.user)
+
+// var obj1={
+//     model:'iphone',
+//     price:70000,
+//     color:'white'
+// }
+
+// var obj2={
+//     model:'Samsung',
+//     price:79000,
+//     color:'white'
+// }
+// var obj3={
+//     model:'nothing',
+//     price:90000,
+//     color:'white'
+// }
+// console.log(obj1);
+// console.log(obj1.model);
+// console.log(obj1.price);
+// console.log(obj1.color);
+// console.log(obj2);
+// console.log(obj3);
+
+// var user ={
+//     userName: 'Prince',
+//     age:69,
+//     greet:function gret(){ //method
+//         console.log("Good morning") 
+//         return 0;
+//     }
+// };
+// // console.log(user)
+// // user.greet();
+// // console.log(user.greet())
+
+// var arr=      //ARRAY OF OBJECTS
+// [  {name:"Prince",age:15},
+//     {name:"Arthank",age:20},
+//     {name:"AAkash",age:22} 
+// ]
+// console.log(arr[0].age);
+
+//----------------DOM---------------
+// DOCUMENT OBJECT MODEL
+// 4-PILLARS OF DOM:
+// I. SELCTION OF AN ELEMENT
+// II. Changing HTML
+//III. CHANGING CSS
+// IV.Event listener
+
+//--I---
+// var h1=document.querySelector('h1');
+// console.log(h1);
+// //---II
+// h1.innerHTML ="Changed"
+// //---III
+// h1.style.color = 'blue'
+// h1.style.backgroundColor="royalblue"
+//---IV---
+// var h1= document.querySelector('h1');
+
+// h1.addEventListener('click',function(){
+//    h1.innerHTML="Changed" })
+ //dblclick
+
+ //--------------------------------------
