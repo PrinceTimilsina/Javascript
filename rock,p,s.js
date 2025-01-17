20cb9ca5c5e5810578c5ccaf26cbb8a0302aa67a
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-17 18:14:05 +0545

    Today's Learnings

---
+++ rock,p,s.js
@@ -0,0 +1,4 @@
+//Rock Paper Scissors is a classic game that resonates with folks from around the world. The rules are as follows:
+//Rock beats Scissors.
+//Scissors beat Paper.
+//Paper beats Rock.
