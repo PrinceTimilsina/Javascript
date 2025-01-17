6df34fd1717eb3080948f98e5a8e308dcde5d121
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-18 05:30:45 +0545

    Today's learnings

---
+++ bmi.js
@@ -0,0 +1,4 @@
+//JS-program that calculates your own BMI(Body mass index)
+let m=50,h=5.6;
+let bmi=m/h**2;
+console.log(bmi);
--- exponents.js
+++ exponents.js
@@ -9,9 +9,4 @@ score = 2 ** 5;      // score is now 32
 
 console.log(score);  // Output: 32
 
-//-Again,next:
 
-//JS-program that calculates your own BMI(Body mass index)
-let m=50,h=5.6;
-let bmi=m/h**2;
-console.log(bmi);
