5983eb78b24219fd0febfdd6d7b9d5b8d1ec87ec
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-18 17:38:23 +0545

    new

---
+++ Example-Programs/friend-Request.js
@@ -0,0 +1,22 @@
+var state = document.querySelector("h5");
+console.log(state);
+var btn = document.querySelector("#add")
+var check = 0;
+btn.addEventListener("click",function(){
+    if(check == 0){
+     state.innerHTML="Friends";
+     state.style.color="Green";
+     console.log("Hey hey hey");
+     btn.innerHTML="Remove";
+     check = 1;
+    }
+    else{
+        state.innerHTML="Stranger";
+        state.style.color="red";
+        btn.innerHTML="Add Friend";
+        check=0;
+    }
+})
+
+
+
