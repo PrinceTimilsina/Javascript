508c66cc9df791bf46e89cfef492de24c9e14665
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-19 15:58:44 +0545

    Today(Magh-6) learnigs

--- Example-Programs/imagehover.js
+++ Example-Programs/imagehover.js
@@ -25,15 +25,16 @@
 //here comes main:
 var elem = document.querySelectorAll(".elem");
 elem.forEach(function(val){
-console.log(val.childNodes)
-
      val.addEventListener("mouseenter",function(){
-       val.style.backgroundColor="red"
+       val.childNodes[3].style.opacity=1  //val.child node - 3 is image img is 3rd child
 
-     })
+     });
 
      val.addEventListener("mouseleave",function(){
-          val.style.backgroundColor="transparent"
-     })
-
-})
+          val.childNodes[3].style.opacity=0
+     });
+    
+     val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left = dets.x + "px"; //style.left: Sets the distance of the element's left edge from its parent container's left edge.
+    });                                               // style.right: Sets the distance of the element's right edge from its parent container's right edge.
+});
