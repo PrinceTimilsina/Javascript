eb34f47d5149f204a3e22b4a034ff1a5030b03b9
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-05-15 15:57:28 +0545

    today's

--- Exciting Projects/script.js
+++ Exciting Projects/script.js
@@ -1,4 +1,4 @@
-let center = document.querySelector("#center")
-center.addEventListener('mousemove',function(dets){
-    console.log(dets.clientX,dets.clientY);
-})
+// let center = document.querySelector("#center")
+// center.addEventListener('mousemove',function(dets){
+//     console.log(dets.clientX,dets.clientY);
+// })
