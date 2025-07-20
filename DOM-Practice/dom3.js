28996b3fd982cbaf686f9b1d446c9e486f1ed52e
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-07-20 11:21:45 +0545

    Update dom3.js

--- DOM-Practice/dom3.js
+++ DOM-Practice/dom3.js
@@ -3,6 +3,6 @@ console.log(a)
 let div1 = a[0].innerHTML="new"
 let div2 = a[1].innerHTML="JS"
 let div3 = a[2].innerHTML="Learnings"
-console.log(div1);
-console.log(div2);
-console.log(div3);
+console.log(div1); //Div-1
+console.log(div2); //Div-2
+console.log(div3); //Div-3
