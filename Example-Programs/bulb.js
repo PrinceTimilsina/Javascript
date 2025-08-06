3621d70b08beb45e0b061c5c07c44bf3d0bf33dc
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2025-08-06 21:06:44 +0545

    Update bulb.js

--- Example-Programs/bulb.js
+++ Example-Programs/bulb.js
@@ -4,7 +4,7 @@ var btn =  document.querySelector("Button")
 var flag = 0;
 btn.addEventListener("click",function(){
     if (flag==0){
-    bulb.style.backgroundColor = "yellow"
+    bulb.style.backgroundColor = "yellow"  
     console.log("ON");
     flag++;
 }
