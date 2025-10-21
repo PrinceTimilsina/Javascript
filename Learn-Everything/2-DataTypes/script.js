d5c5a11d7242346974e9a7bbcb96feec5c17dd19
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-10-21 12:14:21 +0545

    JS

---
+++ Learn-Everything/2-DataTypes/script.js
@@ -0,0 +1,40 @@
+//------------DATA TYPE---------------
+//-----------Primitives & Reference-----
+//Primitives-> all those types of value which gives real values after copying them
+// String,number,boolean,null,undefined,symbol,bigInt
+
+//Reference->While copying these we cannot get real copy but so,we get reference of its parent
+//Arrays,Objects,Functions
+//   []     {}       ()  --game of brackets
+
+
+//-----------STRING-------------
+//''  - single quotes
+//""  - double quotes
+// `` - backticks
+
+//----------NUMBER--------------
+//12 -integer
+//12.3 -integer
+// 0.3 -integer
+
+//----------Boolean-----------
+//let a = true;
+//let b = false;
+
+//----------NULL--------------
+// let selectedStudents = null;
+
+//----------UNDEFINED---------
+//let a; -its by default value is undefined
+
+//----------SYMBOL------------
+// => unique immutable (unchanged) value 
+
+//----------bigInt------------
+//  let a = 1234347957573979373979347937997n;
+//  a = a + 3n
+
+
+//----------Dynamic Typing----------------
+
