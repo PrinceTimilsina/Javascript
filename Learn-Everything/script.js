19337dca1ac6975d1b901954d5841a1554ac4d9d
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-10-21 09:33:36 +0545

    JS-day-1

---
+++ Learn-Everything/script.js
@@ -0,0 +1,24 @@
+//var,let,const-line by line comparion
+// var a;
+// var a = 12;
+// let a;
+// let a = 13;
+// const a = 14;  
+
+//----------------var---------------------------
+
+// var a; //Declaration
+// var a = 10;//Declare and initialization
+
+var a = 12;
+var a = 13;
+//it adds it into window
+//function scoped
+//you can declare it again with same name and it won't show error
+
+//-----------------let-----------------------------
+// let a = 12;
+// let a = 13;
+
+//------------------const---------------------------
+
