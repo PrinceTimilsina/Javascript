7e2c8db603de6c72434df2af0666bc5c78e7197f
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-11-01 09:42:49 +0545

    JS

--- Learn-Everything/3-Operators/script.js
+++ Learn-Everything/3-Operators/script.js
@@ -1,4 +1,23 @@
  //-------------Operators------
  // + - * ** / % = == === <, > <= >= ! != !== && || ?:
+ //----------ARITHMETIC OPERATOR
+ // + :addition,contatination
+ //  -: subtraction
+ // / : division
+ //  * : division
+ // % :modulus i.e. reminder
+ // ** : exponentiation i.e power like 2**3 = 8 i.e. 2^3 = 8,
+//-----------COMPARISON OPERATOR----
 
+// ==     :these compares lhs & rhs 
+  12 == 13 //false
+  12 == "12" //true ------problem with == ,it doesn't checks type,
+
+// ===
+  //but,
+  12==="12"  ///false ----its the right answer
+// !=
+// !==
+// >=
+//<=
  
