8049476a63fd031082308b4abf570459842b69f0
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-11-01 09:49:30 +0545

    JS

--- Learn-Everything/3-Operators/script.js
+++ Learn-Everything/3-Operators/script.js
@@ -9,16 +9,40 @@
  // ** : exponentiation i.e power like 2**3 = 8 i.e. 2^3 = 8,
 //-----------COMPARISON OPERATOR----
 
-// ==     :these compares lhs & rhs 
+// ==     :these compares lhs & rhs ---NOT STRICT COMPARISION
   12 == 13 //false
   12 == "12" //true ------problem with == ,it doesn't checks type,
 
-// ===
+// ===   //STRICT
   //but,
   12==="12"  ///false ----its the right answer
-// !=
+// !=   //NOT STRICT COMPARSION
+   12!="12"  //false
    12!=13 // true   ,basically != is opporsite of ==
-// !==
+// !==   //STRICT 
+   12!=="12" //true
 // >=
 //<=
+//>,<
+
+//----------assignment------------
+// = ,
+  let a=12;
+
+// = 
+// +=
+// -=
+// *=
+// /=
+// %=
+
+let b = 10;
+b+=3; //it means a+3,
+b-+11; //now subtract 11
+b*=2 //now,multiply by 2
+a/=2 //it means divide it by 2
+a%=3//it menas mod of a
+
+
+
  
