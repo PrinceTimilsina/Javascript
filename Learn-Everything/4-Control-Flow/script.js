156e03a156a66c80550d7fb594df671b76e7a854
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-11-07 21:49:19 +0545

    Early Return

--- Learn-Everything/4-Control-Flow/script.js
+++ Learn-Everything/4-Control-Flow/script.js
@@ -28,4 +28,44 @@ function getVal(val){
 
 getVal(45);
 
-//---exercise----
+//---exercise---- 
+
+function getGrade(score){
+	if(score >= 90 && score <=100){
+		return 'A+';
+	}
+	else if(score >= 80 && score <=89){
+		return 'A';
+	}
+
+	else if(score >= 70 && score <=79){
+		return 'B+';
+	}
+
+	else if(score >= 60 && score <=69){
+		return 'B';
+	}
+
+	else if(score >= 50 && score <=59){
+		return 'C+';
+	}
+
+	else if(score >= 40 && score <=49){
+		return 'D+';
+	}
+
+	else if(score>35 && score<40){
+      return 'D';
+	}
+
+	else{
+		return 'Fail';
+	}
+
+}
+
+getGrade(89);
+
+console.log(getGrade(89));
+
+//----qn2----
