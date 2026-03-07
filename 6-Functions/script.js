bd5aefc8b49e65295a5c339cabef19f2ee3f8a03
Prince Timilsina <timilsinaprince07@gmail.com>
2026-03-07 22:14:08 +0545

    Today's learning-functions

--- 6-Functions/script.js
+++ 6-Functions/script.js
@@ -27,9 +27,33 @@ function add(v1,v2){  //--------------->v1,v2 are parameters
 add(2,3);   //------------------------->arguments
 
 
+function add(a,b)
+{
+  console.log(a+b)
+}
+add();
 
+//--when there are more arguments then we need to make that much parameters,so,to protect from it we use rest ...  or its also called spread
+//  if ... is added at function's parameter space then it is rest
+// if... is at arrrays and object then its spread operator
 
+function absd(a,b,c,...val){
+    console.log(a,b,c,val)
+}
 
+absd(1,2,3,4,5,6,7,8)
+
+//return ------
+
+function ans()
+{
+    return 54;
+}
+
+let abc = ans();
+console.log(abc);
+
+//--first class functions
 
 
 
