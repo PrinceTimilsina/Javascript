5a5916f70f22474284629750c2fa7de2439cfbbe
Prince Timilsina <timilsinaprince07@gmail.com>
2026-03-11 19:16:18 +0545

    DPP-1

---
+++ Practise/script1.js
@@ -0,0 +1,6 @@
+//----AFTER LEARNING ALL THE THEORITICAL CONCEPTS,NOW DOING DAILY PRACTISE PROBLEMS
+//Q.1
+let a = 5;
+let b = "5";
+console.log(a==b);  //true,why?because == means loose comparison so,there exist type coersion,
+console.log(a===b); //false,why?becasue === means strict comparison so,there isn't type coersion thus 5 is not equal to "5"
