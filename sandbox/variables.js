279c977c14cfd019c8f8a8667d73460dc800b038
Prince Timilsina <timilsinaprince07@gmail.com>
2026-05-24 17:14:12 +0545

    js

---
+++ sandbox/variables.js
@@ -0,0 +1,23 @@
+// var, let ,const
+
+//code;
+
+//var --->part of ES5
+//let,const-->part of ES6
+
+// var a = 12;
+
+// a = 12; --->bug,
+
+//declaration and initialization
+var a=6; //declaration
+var b=10;//declaration and initialization
+
+//var defects;
+//it is function scoped
+//it adds itself on window
+//can be redeclared without an error
+
+
+
+
