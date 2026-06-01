ebb82d415997f39894078c19ff2e33bcc0f7c0a7
Prince Timilsina <timilsinaprince07@gmail.com>
2026-06-01 22:09:09 +0545

    Daily Learnings

--- DOM/script3.js
+++
@@ -1,9 +0,0 @@
-//function;
-function addSquares(a, b) {
-  function square(x) {
-    return x * x;
-  }
-  return square(a) + square(b);
-}
-
-
---
+++ daily_practice/6-Functions/script2.js
@@ -0,0 +1,40 @@
+function myFunc(theArr) {
+  theArr[0] = 30;
+}
+
+const arr = [45];
+
+console.log(arr[0]); // 45
+myFunc(arr);
+console.log(arr[0]); // 30
+
+
+function sayMyName()
+{
+  console.log("PRINCE");
+}
+sayMyName();
+
+//----Simple Example;
+// function addTwoNUmber(number1,number2)   //parameters
+// {
+//   console.log(number1+number2);
+// }
+// addTwoNUmber(5,9);                  //arguements
+
+function add(num1,num2)
+{
+  let result=num1+num2;
+  console.log(result);
+}
+add(4,7);
+
+//SQuare;
+function addSquares(a, b) {
+  function square(x) {
+    return x * x;
+  }
+  return square(a) + square(b);
+}
+
+
