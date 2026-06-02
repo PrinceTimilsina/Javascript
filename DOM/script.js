f8b724be15813fbec05a86fd2e19842aaa729b84
Prince Timilsina <timilsinaprince07@gmail.com>
2026-06-02 22:07:13 +0545

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,8 @@
-var a = document.querySelector("h1")
-console.log(a);
+console.log("JS CONNECTED"); //to ensure proper connection
+let heading=document.getElementById("title");
+console.log(heading);
+
+let button=document.getElementById("btn");
+console.log(button)
+
+heading.textContent="I changed this using JS"
