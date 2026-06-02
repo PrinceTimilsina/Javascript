f8a5e0eecada156f51f294d096f84761d2164c9b
Prince Timilsina <timilsinaprince07@gmail.com>
2026-06-02 23:00:50 +0545

    DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,8 +1,21 @@
 console.log("JS CONNECTED"); //to ensure proper connection
+//CONNECTING DOCUMENT WITH JS;
+
 let heading=document.getElementById("title");
 console.log(heading);
 
 let button=document.getElementById("btn");
 console.log(button)
 
+//STYLING USING JS;
+
 heading.textContent="I changed this using JS"
+heading.style.color="red"
+heading.style.fontSize="100px"
+
+//EVENT LISTENER;
+
+button.addEventListener("click",function(){
+heading.textContent="Button clicked";
+});
+
